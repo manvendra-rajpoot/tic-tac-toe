fee1794a2a826dca06deb5d70fc3a1bf1b575919
Manvendra Rajpoot <manvendra141986@gmail.com>
2020-10-28 16:44:37 +0530

    css layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
+
+const Square = (props) => {
+  return( 
+    <div className="square">
+      {props.value}
+    </div>
+  );
+};
+const Board = () => {
+  const renderSquare = (i) => {
+    return(
+      <Square value={i} />
+    );
+  };
+  return( 
+    <div style = {
+      {
+        backgroundColor: "skyblue",
+        margin: 10,
+        padding: 20,
+      }
+    }>
+      Board
+      <div className = "board-row">
+        {renderSquare('X')}{renderSquare('O')}{renderSquare()}
+      </div>
+      <div className = "board-row">
+        {renderSquare('O')}{renderSquare()}{renderSquare()}
+      </div>
+      <div className = "board-row">
+        {renderSquare()}{renderSquare('X')}{renderSquare()}
+      </div>
+    </div>
+  );
+};
 const Game = () => {
   return( 
-    <div>
-      Tic-Tac-Toe Game!
+    <div className = "game">
+      Tic-Tac-Toe Game
+      <Board/>
     </div>
   );
 };
